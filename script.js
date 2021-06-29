e846754a73f10cc7ad4acefa086198fd3b6134f9
Manuel Rico <ds23906@gmail.com>
2021-06-28 22:22:57 -0600

    final-project

--- script.js
+++ script.js
@@ -14,132 +14,136 @@ let priority_ = document.querySelectorAll('.priority-text');
 const selectOrder = document.getElementById('select-order');
 
 btnNewTask.addEventListener("click", () => {
-    taskListContainer.classList.remove("show");
-    informationContainer.classList.remove("hide");
+  taskListContainer.classList.remove("show");
+  informationContainer.classList.remove("hide");
 });
 
 btnList.addEventListener("click", () => {
-    informationContainer.classList.add("hide");
-    taskListContainer.classList.add("show");
-    task = document.querySelectorAll('.task-container');
-    priority_ = document.querySelectorAll('.priority-text');
-    console.log(task);
-    console.log(priority_);
-    orderElements(selectOrder.value);
+  informationContainer.classList.add("hide");
+  taskListContainer.classList.add("show");
+  task = document.querySelectorAll('.task-container');
+  priority_ = document.querySelectorAll('.priority-text');
+  console.log(task);
+  console.log(priority_);
+  orderElements(selectOrder.value);
 });
 
-btnSave.addEventListener("click",() =>{
-    if (isDataCorrect()) buildTask();
-    emptyTxt();
+btnSave.addEventListener("click", () => {
+  if (isDataCorrect()) buildTask();
+  emptyTxt();
 });
 
-function setPriority (){
+function setPriority(headerContainer) {
   let str = "";
   txtPriority.forEach((element) => {
-      if(element.checked){
-          str = element.value;
-      } 
+    if (element.checked) {
+      str = element.value;
+    }
   });
+  if (str === "HIGH") headerContainer.classList.add("red");
+  else if (str === "MEDIUM") headerContainer.classList.add("yellow");
+  else headerContainer.classList.add("green");
   return str;
 }
 
-function isDataCorrect (){
+function isDataCorrect() {
   console.log(txtComment.value);
-   if (txtTittle.value.trim() != ""){
-     if (txtHour.value != ""){
-      if (txtComment.value.trim() != ""){
+  if (txtTittle.value.trim() != "") {
+    if (txtHour.value != "") {
+      if (txtComment.value.trim() != "") {
         return true;
-       }   
-     }
-   }
-   return false;
+      }
+    }
+  }
+  return false;
 }
 
-function emptyTxt () {
+function emptyTxt() {
   txtTittle.value = "";
   txtComment.value = "";
   txtHour.value = "";
 }
 
 ////////////////////////////////////
-function buildTask () {
-    
-let taskContainer = document.createElement("div");
-let headerTaskContainer = document.createElement("div");
-let mainTaskContainer = document.createElement("div");
-let hour = document.createElement("p");
-let priority = document.createElement("p");
-let tittle = document.createElement("p");
-let line = document.createElement("hr");
-let comment = document.createElement("p");
+function buildTask() {
 
-taskContainer.classList.add("task-container");
-headerTaskContainer.classList.add("header-task-container");
-hour.classList.add("hour-text");
-priority.classList.add("priority-text");
-mainTaskContainer.classList.add("main-task-container");
+  let taskContainer = document.createElement("div");
+  let headerTaskContainer = document.createElement("div");
+  let mainTaskContainer = document.createElement("div");
+  let hour = document.createElement("p");
+  let priority = document.createElement("p");
+  let tittle = document.createElement("p");
+  let line = document.createElement("hr");
+  let comment = document.createElement("p");
 
-hour.innerText = txtHour.value;
-priority.innerText = setPriority();
-tittle.innerText = txtTittle.value;
-comment.innerText = txtComment.value;
+  taskContainer.classList.add("task-container");
+  headerTaskContainer.classList.add("header-task-container");
+  hour.classList.add("hour-text", "task-text-list");
+  priority.classList.add("priority-text", "task-text-list");
+  tittle.classList.add("task-text-list", "tittle-text");
+  comment.classList.add("task-text-list");
+  mainTaskContainer.classList.add("main-task-container");
 
-taskListContainer.appendChild(taskContainer);
-taskContainer.appendChild(headerTaskContainer);
-taskContainer.appendChild(mainTaskContainer);
+  hour.innerText = txtHour.value;
+  priority.innerText = setPriority(headerTaskContainer);
+  tittle.innerText = txtTittle.value;
+  comment.innerText = txtComment.value;
 
-let headerElement = [hour,priority];
-let mainElement = [tittle,line,comment];
+  taskListContainer.appendChild(taskContainer);
+  taskContainer.appendChild(headerTaskContainer);
+  taskContainer.appendChild(mainTaskContainer);
 
-headerElement.forEach((element) => {
-  headerTaskContainer.appendChild(element);
-});
+  let headerElement = [hour, priority];
+  let mainElement = [tittle, line, comment];
 
-mainElement.forEach((element) => {
-  mainTaskContainer.appendChild(element);
-});
+  headerElement.forEach((element) => {
+    headerTaskContainer.appendChild(element);
+  });
+
+  mainElement.forEach((element) => {
+    mainTaskContainer.appendChild(element);
+  });
 
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 selectOrder.addEventListener("change", () => {
-    orderElements(selectOrder.value);
+  orderElements(selectOrder.value);
+});
+
+function orderElements(str) {
+  let cont = 0;
+  priority_.forEach(element => {
+    task[cont].classList.remove("hide");
+    task[cont].classList.remove("third");
+    task[cont].classList.remove("second");
+    task[cont].classList.remove("first");
+
+    if (str === "From HIGH to LOW") {
+      if (element.innerText === "HIGH") task[cont].classList.add("first");
+      if (element.innerText === "MEDIUM") task[cont].classList.add("second");
+      if (element.innerText === "LOW") task[cont].classList.add("third");
+    }
+
+    if (str === "From LOW to HIGH") {
+      if (element.innerText === "HIGH") task[cont].classList.add("third");
+      if (element.innerText === "MEDIUM") task[cont].classList.add("second");
+      if (element.innerText === "LOW") task[cont].classList.add("first");
+    }
+
+    if (str === "HIGH") {
+      if (element.innerText != "HIGH") task[cont].classList.add("hide");
+    }
+
+    if (str === "MEDIUM") {
+      if (element.innerText != "MEDIUM") task[cont].classList.add("hide");
+    }
+
+    if (str === "LOW") {
+      if (element.innerText != "LOW") task[cont].classList.add("hide");
+    }
+
+    cont++;
   });
-  
-  function orderElements (str) {
-    let cont = 0;
-    priority_.forEach(element => {
-      task[cont].classList.remove("hide");
-      task[cont].classList.remove("third");
-      task[cont].classList.remove("second");
-      task[cont].classList.remove("first");
-      
-       if(str === "From HIGH to LOW"){  
-        if (element.innerText ==="HIGH") task[cont].classList.add("first");
-         if (element.innerText ==="MEDIUM") task[cont].classList.add("second");
-          if (element.innerText ==="LOW") task[cont].classList.add("third"); 
-      }
-      
-        if(str === "From LOW to HIGH"){  
-          if (element.innerText ==="HIGH") task[cont].classList.add("third");
-           if (element.innerText ==="MEDIUM") task[cont].classList.add("second");
-             if (element.innerText ==="LOW") task[cont].classList.add("first"); 
-      }
-      
-      if(str === "HIGH"){  
-        if (element.innerText !="HIGH") task[cont].classList.add("hide");
-      }
-      
-      if(str === "MEDIUM"){
-        if (element.innerText !="MEDIUM") task[cont].classList.add("hide");
-      }
-      
-      if(str === "LOW"){
-        if (element.innerText != "LOW") task[cont].classList.add("hide");
-      }
-      
-      cont++;
-    });
-  }
-   
-  
+}
+
